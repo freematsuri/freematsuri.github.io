ebdd884d09707c89b2bfade8d79c1a03b2e2a094
mdm2e@hotmail.com <mdm2e@hotmail.com>
2024-12-22 17:31:25 +0800

    hen bu wen he de chan pin shi tong ji bie chan pin zhong qian suo wei jian de xing neng shou ji xin biao

---
+++ assets/website/js/frontend/freematsuri/dist/active-switcher.js
@@ -0,0 +1,35 @@
+
+var colorSheets = [
+    {
+        color: "#e4353a",
+        title: "Switch to Default",
+        href: "./assets/css/color/color-default.css"
+    },
+    {
+        color: "#9dc02e",
+        title: "Switch to #9dc02e",
+        href: "./assets/css/color/color2.css"
+    },
+    {
+        color: "#cf9455",
+        title: "Switch to #cf9455",
+        href: "./assets/css/color/color3.css"
+    },
+    {
+        color: "#65ae84",
+        title: "Switch to #65ae84",
+        href: "./assets/css/color/color4.css"
+    },
+    {
+        color: "#5b69cb",
+        title: "Switch to #5b69cb",
+        href: "./assets/css/color/color5.css"
+    },
+    {
+        color: "#0a35af",
+        title: "Switch to #0a35af",
+        href: "./assets/css/color/color6.css"
+    }
+];
+
+ColorSwitcher.init(colorSheets);
